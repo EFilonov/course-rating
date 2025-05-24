fe751504635a9f8cede69bf0437953268ba7b013
Evgeniy Y. Filonov <evgeniyfilonov@gmail.com>
2025-05-24 19:29:32 +0300

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,76 @@
+import pluginJs from '@eslint/js';
+import nextPlugin from '@next/eslint-plugin-next';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import importPlugin from 'eslint-plugin-import';
+import pluginPromise from 'eslint-plugin-promise';
+import pluginReact from 'eslint-plugin-react';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  importPlugin.flatConfigs.recommended,
+  pluginPromise.configs['flat/recommended'],
+  pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat['jsx-runtime'],
+  eslintConfigPrettier,
+
+  {
+    rules: {
+      // ✅ Точка с запятой
+      'semi': ['warn', 'always'],
+
+      // React rules
+      'react/react-in-jsx-scope': 'off',
+      'react-hooks/exhaustive-deps': 'off',
+      'react/display-name': 'off',
+      'react/prop-types': 'off',
+
+      // TS rules
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-expressions': 'off',
+
+      // Import rules
+      'import/no-unresolved': 'off',
+      'import/no-named-as-default': 'off',
+
+      // Style
+      'newline-before-return': 'error',
+    },
+  },
+
+  {
+    // ✅ Next.js community workaround
+    plugins: {
+      '@next/next': nextPlugin,
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs['core-web-vitals'].rules,
+      '@next/next/no-img-element': 'off',
+    },
+  },
+
+  {
+    ignores: ['.next/*'],
+  },
+];
