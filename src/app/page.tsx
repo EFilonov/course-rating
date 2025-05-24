31b9b67b87d90e9e2999734a66af38590ebd16fa
Evgeniy Y. Filonov <evgeniyfilonov@gmail.com>
2025-05-24 23:30:54 +0300

    starter: debug otions  added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
-import HomePage from '@/app/(delete-this-and-modify-page.tsx)/HomePage';
+// import HomePage from '@/app/(delete-this-and-modify-page.tsx)/HomePage';
+"use server";
+import React from 'react';
+import cn from 'classnames';
 
 /**
  * The main page component that renders the HomePage component.
@@ -6,7 +9,21 @@ import HomePage from '@/app/(delete-this-and-modify-page.tsx)/HomePage';
  * @returns {JSX.Element} The rendered HomePage component.
  */
 const Page = () => {
-    return <HomePage />;
+
+    const aasdasd:number = 5; // Example variable to demonstrate TypeScript usage
+
+    // const [hovering, setHovering] = React.useState(false);
+
+    return <div 
+        // onMouseOver={() => setHovering(true)}
+        // onMouseOut={() => setHovering(false)} 
+        // className={cn('container', {'mx-auto': hovering}, 'p-4')}
+        >
+        <h1>Welcome to the Home Page !!!{aasdasd}</h1>
+        {/* Uncomment the line below to include the HomePage component */}
+        {/* <HomePage /> */}
+        <p>This is the main page of the application.</p>    
+    </div>;
 };
 
 export default Page; 
