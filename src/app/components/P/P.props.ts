25c7200ab3eba95b45b1635a2165f85737dd1388
Evgeniy Y. Filonov <evgeniyfilonov@gmail.com>
2025-05-27 19:03:40 +0300

    fix: P changes

--- src/app/components/P/P.props.ts
+++ src/app/components/P/P.props.ts
@@ -3,8 +3,5 @@ import {ReactNode} from "react";
 export interface PProps {
     fontSize?: 'small' | 'regular' | 'large';
     children: ReactNode;
-<<<<<<< HEAD
     className?: string;
-=======
->>>>>>> 1fad826fba0326cc2773ac5134b01ab3cfea5c61
 }
--- src/app/components/P/P.tsx
+++ src/app/components/P/P.tsx
@@ -1,6 +1,6 @@
 import {PProps} from "./P.props";
 import {JSX} from "react";
-<<<<<<< HEAD
+
 import cn from 'classnames';
 import style from './p.module.css';
 
@@ -11,15 +11,7 @@ const P = ({ fontSize = 'regular', className, children, ...props }: PProps): JSX
         >
 
             {children}
-            
-=======
-import style from './p.module.css';
 
-const P = ({ fontSize = 'regular', children }: PProps): JSX.Element  => {
-    return (
-        <p className={style[fontSize]}>
-            {children}
->>>>>>> 1fad826fba0326cc2773ac5134b01ab3cfea5c61
         </p>    
     );
 };
