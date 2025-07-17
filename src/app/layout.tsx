6b12d779163ca14431da276a7c3ca866cf2bebd6
Evgeniy Y. Filonov <evgeniyfilonov@gmail.com>
2025-07-17 18:35:36 +0300

    Minor metadata refactoring

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-
 import type { ReactNode } from 'react';
 import type { Metadata } from 'next';
 import { Roboto} from 'next/font/google';
@@ -7,6 +6,23 @@ import './globals.css';
 export const metadata: Metadata = {
     title: 'Eugene Next.js App',
     description: 'A Next.js application with custom fonts and themes.',
+    openGraph: {
+        title: 'Приложениие для оценки курсов',
+        description: 'Читай отзывы, оцени курсы и делись своим мнением',
+        images: [
+            {
+                url: 'https://evgens-rating.vercel.app/images/openGraph.png',
+                width: 1200,
+                height: 630,
+                alt: 'Cource rating by Evgeniy Filonov'
+            }
+        ],
+        url: 'https://evgens-rating.vercel.app/',
+        type: 'website',
+        siteName: 'Cource rating by Evgeniy Filonov',
+        locale: 'ru_RU',
+        
+    },
 };
 
 const roboto = Roboto({
